d8af6ab05c0eac0c89486b884f4f49c7b8de3438
joshdrink <git@joshbeveridge.ca>
2019-10-29 16:40:39 -0400

    updates the form to include basic components

--- app/js/app.js
+++ app/js/app.js
@@ -78,3 +78,116 @@ $("[data-gc-legal-name-different-trigger]").on("click", function() {
 });
 
     // Repeater ----------------------------------------------------------------
+
+// Citizenship =================================================================
+$("[data-gc-citizen-true-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        // Remove alternative option's stuff.
+        $("[data-gc-citizen-false-trigger]").removeClass("active");
+        $("[data-gc-citizen-false]").addClass("hidden");
+    }
+});
+
+$("[data-gc-citizen-false-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-citizen-false]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-citizen-true-trigger]").removeClass("active");
+        $("[data-gc-citizen-true]").addClass("hidden");
+    }
+});
+
+// Multiple Citizenship ========================================================
+$("[data-gc-multiple-citizen-true-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-multiple-citizen-true]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-multiple-citizen-false-trigger]").removeClass("active");
+        $("[data-gc-multiple-citizen-false]").addClass("hidden");
+    }
+});
+
+$("[data-gc-multiple-citizen-false-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        // Remove alternative option's stuff.
+        $("[data-gc-multiple-citizen-true-trigger]").removeClass("active");
+        $("[data-gc-multiple-citizen-true]").addClass("hidden");
+    }
+});
+
+// Born Outside the Country ====================================================
+$("[data-gc-home-select]").on("click", function() {
+    if ($(this).val() === "Canada") {
+        $("[data-gc-foreign-born]").addClass("hidden");
+    } else {
+        $("[data-gc-foreign-born]").removeClass("hidden");
+    }
+});
+
+// Naturalization ==============================================================
+$("[data-gc-naturalization-true-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-naturalization-true]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-naturalization-false-trigger]").removeClass("active");
+        $("[data-gc-naturalization-false]").addClass("hidden");
+    }
+});
+
+$("[data-gc-naturalization-false-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-naturalization-false]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-naturalization-true-trigger]").removeClass("active");
+        $("[data-gc-naturalization-true]").addClass("hidden");
+    }
+});
+
+// Naturalization w/ Citizenship ===============================================
+$("[data-gc-naturalization-application-true-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-naturalization-application-true]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-naturalization-application-false-trigger]").removeClass("active");
+        $("[data-gc-naturalization-application-false]").addClass("hidden");
+    }
+});
+
+$("[data-gc-naturalization-application-false-trigger]").on("click", function() {
+    if ($(this).hasClass("active")) {
+
+    } else {
+        // Triger this elements stuff.
+        $(this).addClass("active");
+        $("[data-gc-naturalization-application-false]").removeClass("hidden");
+        // Remove alternative option's stuff.
+        $("[data-gc-naturalization-application-true-trigger]").removeClass("active");
+        $("[data-gc-naturalization-application-true]").addClass("hidden");
+    }
+});
